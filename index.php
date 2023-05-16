36b5ad116bc0c72aea504c653ecf64f7d22c6f2a
joris-landaret <joris.landaret@laplateforme.io>
2023-05-16 11:11:05 +0200

    installing the router

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$router = new AltoRouter();
+
+$match = $router->match();
+
+if (is_array($match) && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+} else {
+    // no route was matched
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
