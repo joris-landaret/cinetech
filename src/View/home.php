f485e16bbad44c3a28c3e1fbcdb5a1ab6781b6e2
joris-landaret <joris.landaret@laplateforme.io>
2023-05-24 15:36:47 +0200

    add some style to the movie

--- src/View/home.php
+++ src/View/home.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
     <title>Document</title>
 </head>
 
@@ -14,7 +15,7 @@
     ?>
     <main id="main">
         <div class="movie">
-            <img src="" alt="image">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/b/be/Film_strip.jpg" alt="image">
 
             <div class="movie-info">
                 <h3>Titre du film</h3>
@@ -22,12 +23,14 @@
             </div>
 
             <div class="overview">
+
+                <h3>Description</h3>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus architecto quod perferendis. Voluptate, impedit tempore doloremque fugit esse aspernatur? Sit nam itaque non modi cupiditate provident minus odio deserunt! Quo?
             </div>
         </div>
 
         <div class="movie">
-            <img src="" alt="image">
+            <img src="https://thumbs.dreamstime.com/b/ardoise-de-film-et-bobine-de-film-sur-le-bois-36502412.jpg" alt="image">
 
             <div class="movie-info">
                 <h3>Titre du film</h3>
