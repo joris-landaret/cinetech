a5679e905fea66cb76cebcebb5ec0787afa74615
joris-landaret <joris.landaret@laplateforme.io>
2023-05-26 09:38:03 +0200

    the films are displayed on the home page but I'm changing to the films page

--- src/View/home.php
+++ src/View/home.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
-    <title>Document</title>
+    <script defer src="style/movie.js"></script>
+    <title>Cinetech</title>
 </head>
 
 <body>
@@ -25,6 +26,19 @@
             <div class="overview">
 
                 <h3>Description</h3>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus architecto quod perferendis. Voluptate, impedit tempore doloremque fugit esse aspernatur? Sit nam itaque non modi cupiditate provident minus odio deserunt! Quo?
+            </div>
+        </div>
+
+        <div class="movie">
+            <img src="https://thumbs.dreamstime.com/b/ardoise-de-film-et-bobine-de-film-sur-le-bois-36502412.jpg" alt="image">
+
+            <div class="movie-info">
+                <h3>Titre du film</h3>
+                <span class="green">9.8</span>
+            </div>
+
+            <div class="overview">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus architecto quod perferendis. Voluptate, impedit tempore doloremque fugit esse aspernatur? Sit nam itaque non modi cupiditate provident minus odio deserunt! Quo?
             </div>
         </div>
