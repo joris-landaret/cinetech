e683bf2e5cc1da65c8ec7e4d8c1db96236ca6c2c
joris-landaret <joris.landaret@laplateforme.io>
2023-05-26 09:47:55 +0200

    the films are displayed and the search bar works.

--- src/View/movies.php
+++ src/View/movies.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <script defer src="style/movie.js"></script>
     <title>Document</title>
 </head>
 
@@ -12,6 +14,11 @@
     <?php
     require_once 'src/view/header.php';
     ?>
+    <main id="main">
+        <div class="movie">
+
+        </div>
+    </main>
 </body>
 
 </html>
